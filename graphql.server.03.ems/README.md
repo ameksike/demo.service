8e8c02ae19220faec439e6015db86b1da4e215c9
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-10-01 22:39:08 +0200

    update query demo

--- graphql.server.03.ems/README.md
+++ graphql.server.03.ems/README.md
@@ -1,6 +1,26 @@
+### Client
 
 
-- References
-    - [Getting Started With GraphQL.js](https://graphql.org/graphql-js/)
-    - [Running Express + GraphQL](https://graphql.org/graphql-js/running-an-express-graphql-server/)
-    - [Build and Deploy a GraphQL API using NodeJS](https://www.youtube.com/watch?v=UYQSVH6B1k4)
+
+`POST http://localhost:4004/graphql`
+```js
+{
+    "query": `
+        query MyQuery($name: String) { 
+            hello(name: $name) 
+            weight 
+            age 
+            hobbies
+        }
+    `,
+    "variables": { 
+        "name": "Demo1" 
+    }
+}
+```
+
+
+### References
+- [Getting Started With GraphQL.js](https://graphql.org/graphql-js/)
+- [Running Express + GraphQL](https://graphql.org/graphql-js/running-an-express-graphql-server/)
+- [Build and Deploy a GraphQL API using NodeJS](https://www.youtube.com/watch?v=UYQSVH6B1k4)
--- graphql.server.03.ems/src/index.js
+++ graphql.server.03.ems/src/index.js
@@ -1,1 +1,49 @@
-console.log("dede")
+import express from "express"
+import { createHandler } from "graphql-http/lib/use/express"
+import { buildSchema } from "graphql"
+import { ruruHTML } from "ruru/server"
+
+
+// Construct a schema, using GraphQL schema language
+const schema = buildSchema(`
+  type Query {
+    hello(name: String): String
+
+    age: Int
+    weight: Float!
+    isOver: Boolean
+    hobbies: [String!]!
+  }
+`)
+
+// The root provides a resolver function for each API endpoint
+const root = {
+    hello({ name }) {
+        return "Hello " + name
+    },
+    age: 17,
+    weight: 34.5,
+    isOver: true,
+    hobbies: ["one", "two"]
+}
+
+const app = express()
+
+// Create and use the GraphQL handler.
+app.all(
+    "/graphql",
+    createHandler({
+        schema: schema,
+        rootValue: root,
+    })
+)
+
+// Serve the GraphiQL IDE.
+app.get("/", (_req, res) => {
+    res.type("html")
+    res.end(ruruHTML({ endpoint: "/graphql" }))
+})
+
+// Start the server at port
+app.listen(4004)
+console.log("Running a GraphQL API server at http://localhost:4004/graphql")
