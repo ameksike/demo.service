6890e6276c9d0cfc9439ac592ff4035bc1b406a3
Antonio Membrides Espinosa <tonykssa@gmail.com>
2024-10-01 23:41:05 +0200

    add custom resolvers based in it hierarchy

--- graphql.server.03.ems/src/model/default.schema_service.js
+++ graphql.server.03.ems/src/model/default.schema_service.js
@@ -1,4 +1,20 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+import { GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
+
+const User = new GraphQLObjectType({
+    name: "User",
+    fields: {
+        id: {
+            type: GraphQLInt
+        },
+        name: {
+            type: GraphQLString,
+            // Using custom resolvers based in it hierarchy
+            resolve: (obj) => {
+                return obj.name?.trim().toUpperCase()
+            }
+        }
+    }
+});
 
 export const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
@@ -9,6 +25,15 @@ export const schema = new GraphQLSchema({
                 resolve: (args) => {
                     const { name = "tst" } = args || {};
                     return "Hello " + name
+                }
+            },
+            user: {
+                type: User,
+                resolve: () => {
+                    return {
+                        id: 1,
+                        name: "Vadim"
+                    }
                 }
             }
         }
